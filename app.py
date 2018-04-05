cc7530852561a2fe25452c521e5143c0730e1300
BooLuON <toyofumi1223@gmail.com>
2018-04-05 21:23:38 +0900

    Init

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -6,224 +8,169 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+)
+
+import requests
+from bs4 import BeautifulSoup   
 
 app = Flask(__name__)
 
+# Channel Access Token
+line_bot_api = LineBotApi('AzH87TcvfQKLWSRFu2ziKsU10gIyDwK5TeMlgmnzoF+M3WAW1Jdtvmjq/BlF4wqlYNicqEJ3QzmReL7EQ5/dbxlNNROsVLMNMAZ6yJquRVTTNEG2msiups++EZJmHrjj2cPYsIs2O2tpiCFykonofgdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('d4ad2ad65af463cd625172cb419f50e0')
 
-line_bot_api = LineBotApi('b0oOxY7hM4rb8AGClOYmKtPqCIFK004tf55cbZ3QZZNr+/bUZEKve1+flyNG//vRjMHK5xt6XLZcqhrs6B8v5gW6dKC5BIb26wDDfUCekK5AbObpdycypf49s+v3r3IqWVqRWvoQ267sLm2Rk4BO/AdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('33741068f54f5270a576abb88087fc3a')
 
-@app.route("/", methods=['GET'])
-def hello():
-    return "Hello World!"
-
-@app.route("/", methods=['POST'])
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: " + body, "Signature: " + signature)
+    app.logger.info("Request body: " + body)
+
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-       abort(400)
+        abort(400)
 
     return 'OK'
 
 
+def get_page_number(content):
+    start_index = content.find('index')
+    end_index = content.find('.html')
+    page_number = content[start_index + 5: end_index]
+    return int(page_number) + 1
+
+
+def crewl_page(res, push_rate):
+    soup_ = BeautifulSoup(res.text, 'lxml')
+    article_seq = []
+    for r_ent in soup_.find_all(class_="recipes-sort"):
+        try:
+            # 先得到每篇文章的篇url
+            link = r_ent.find('a')['href']
+            if link:
+                # 確定得到url再去抓 標題 以及 推文數
+                title = r_ent.find(class_="title").text.strip()
+                rate = r_ent.find('data-count', class_="fav-count recipe-favorites").text
+                url = 'https://icook.tw/' + link
+
+                # 比對推文數
+                if int(rate) >= 20:
+                    article_seq.append({
+                        'title': title,
+                        'url': url,
+                        'rate': rate,
+                    })
+        except Exception as e:
+            # print('crawPage function error:',r_ent.find(class_="title").text.strip())
+            print('本文已被刪除', e)
+    return article_seq
+
+
+r = requests.get('https://book.douban.com/subject/1084336/comments/')
+soup = BeautifulSoup(r.text, 'lxml')
+pattern = soup.find_all('p', 'comment-content')
+
+
+def crewl_icook(res, push_rate):
+    r = 'https://icook.tw/recipes/search?q={}&ingredients='.format(res)
+    soup = BeautifulSoup(r.text, 'lxml')
+    article_icook_seq = []
+    for r_ent in soup.find_all(class_="recipes-sort"):
+        try:
+            # 先得到每篇文章的篇url
+            link = r_ent.find('a')['href']
+
+            if link:
+                # 確定得到url再去抓 標題 以及 推文數
+                title = r_ent.find(class_="title").text.strip()
+                url_link = 'https://icook.tw/' + link
+                article_icook_seq.append({
+                    'url_link': url_link,
+                    'title': title
+                })
+
+        except Exception as e:
+            # print u'crawPage function error:',r_ent.find(class_="title").text.strip()
+            # print('本文已被刪除')
+            print('delete', e)
+    return article_icook_seq
+
+
+def icook():
+    rs = requests.session()
+    load = {
+        'from': 'https://icook.tw/?ref=nav',
+        'yes': 'yes'
+    }
+    res = rs.post('https://icook.tw', verify=False, data=load)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    all_page_url = soup.select('.btn.wide')[1]['href']
+    start_page = get_page_number(all_page_url)
+    index_list = []
+    article_icook = []
+    for page in range(start_page, start_page - 2, -1):
+        page_url = 'https://icook.tw/recipes/search?q={}&ingredients='.format(page)
+        index_list.append(page_url)
+
+    # 抓取 文章標題 網址 推文數
+    while index_list:
+        index = index_list.pop(0)
+        res = rs.get(index, verify=False)
+        # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+        if res.status_code != 200:
+            index_list.append(index)
+            # print u'error_URL:',index
+            # time.sleep(1)
+        else:
+            article_icook = crewl_icook(res)
+            # print u'OK_URL:', index
+            # time.sleep(0.05)
+    content = ''
+    for index, article in enumerate(article_icook, 0):
+        if index == 15:
+            return content
+        data = '{}\n{}\n\n'.format(article.get('title', None), article.get('url_link', None))
+        content += data
+    return content
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    #print(type(msg))
-    msg = msg.encode('utf-8')  
-    if event.message.text == "文字":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
-    elif event.message.text == "貼圖":
-        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=1, sticker_id=2))
-    elif event.message.text == "圖片":
-        line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='圖片網址', preview_image_url='圖片網址'))
-    elif event.message.text == "影片":
-        line_bot_api.reply_message(event.reply_token,VideoSendMessage(original_content_url='影片網址', preview_image_url='預覽圖片網址'))
-    elif event.message.text == "音訊":
-        line_bot_api.reply_message(event.reply_token,AudioSendMessage(original_content_url='音訊網址', duration=100000))
-    elif event.message.text == "位置":
-        line_bot_api.reply_message(event.reply_token,LocationSendMessage(title='my location', address='Tainan', latitude=22.994821, longitude=120.196452))
-    elif event.message.text == "位置2":
-        imagemap_message = ImagemapSendMessage(
-                        base_url='',
-                        alt_text='this is an imagemap',
-                        base_size=BaseSize(height=520, width=520),
-                        actions=[
-                            URIImagemapAction(
-                                link_uri='',
-                                area=ImagemapArea(
-                                    x=174, y=65, width=707, height=416
-                                )
-                            ),
-                            MessageImagemapAction(
-                                text='hello',
-                                area=ImagemapArea(
-                                    x=520, y=0, width=520, height=520
-                                )
-                            )
-                        ]
-                    )
-        line_bot_api.reply_message(event.reply_token,imagemap_message)
-    elif event.message.text == "樣板":    
-        buttons_template = TemplateSendMessage(
-        alt_text='目錄 template',
-        template=ButtonsTemplate(
-            title='Template-樣板介紹',
-            text='Template分為四種，也就是以下四種：',
-            thumbnail_image_url='圖片網址',
-            actions=[
-                MessageTemplateAction(
-                    label='Buttons Template',
-                    text='Buttons Template'
-                ),
-                MessageTemplateAction(
-                    label='Confirm template',
-                    text='Confirm template'
-                ),
-                MessageTemplateAction(
-                    label='Carousel template',
-                    text='Carousel template'
-                ),
-                MessageTemplateAction(
-                    label='Image Carousel',
-                    text='Image Carousel'
-                )
-            ]
-        )
-    )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
-    elif event.message.text == "Buttons Template":       
-        buttons_template = TemplateSendMessage(
-        alt_text='Buttons Template',
-        template=ButtonsTemplate(
-            title='這是ButtonsTemplate',
-            text='ButtonsTemplate可以傳送text,uri',
-            thumbnail_image_url='圖片網址',
-            actions=[
-                MessageTemplateAction(
-                    label='ButtonsTemplate',
-                    text='ButtonsTemplate'
-                ),
-                URITemplateAction(
-                    label='VIDEO1',
-                    uri='影片網址'
-                ),
-                PostbackTemplateAction(
-                label='postback',
-                text='postback text',
-                data='postback1'
-                )
-            ]
-        )
-    )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
-    elif event.message.text == "Carousel template":
-        print("Carousel template")       
-        Carousel_template = TemplateSendMessage(
-        alt_text='目錄 template',
-        template=CarouselTemplate(
-        columns=[
-            CarouselColumn(
-                thumbnail_image_url='圖片網址',
-                title='this is menu1',
-                text='description1',
-                actions=[
-                    PostbackTemplateAction(
-                        label='postback1',
-                        text='postback text1',
-                        data='action=buy&itemid=1'
-                    ),
-                    MessageTemplateAction(
-                        label='message1',
-                        text='message text1'
-                    ),
-                    URITemplateAction(
-                        label='uri1',
-                        uri='網址'
-                    )
-                ]
-            ),
-            CarouselColumn(
-                thumbnail_image_url='圖片網址',
-                title='this is menu2',
-                text='description2',
-                actions=[
-                    PostbackTemplateAction(
-                        label='postback2',
-                        text='postback text2',
-                        data='action=buy&itemid=2'
-                    ),
-                    MessageTemplateAction(
-                        label='message2',
-                        text='message text2'
-                    ),
-                    URITemplateAction(
-                        label='連結2',
-                        uri='網址'
-                    )
-                ]
-            )
-        ]
-    )
-    )
-        line_bot_api.reply_message(event.reply_token,Carousel_template)
-    elif event.message.text == "Confirm template":
-        print("Confirm template")       
-        Confirm_template = TemplateSendMessage(
-        alt_text='目錄 template',
-        template=ConfirmTemplate(
-            title='這是ConfirmTemplate',
-            text='這就是ConfirmTemplate,用於兩種按鈕選擇',
-            actions=[                              
-                PostbackTemplateAction(
-                    label='Y',
-                    text='Y',
-                    data='action=buy&itemid=1'
-                ),
-                MessageTemplateAction(
-                    label='N',
-                    text='N'
-                )
-            ]
-        )
-    )
-        line_bot_api.reply_message(event.reply_token,Confirm_template)
-    elif event.message.text == "Image Carousel":
-        print("Image Carousel")       
-        Image_Carousel = TemplateSendMessage(
-        alt_text='Image Carousel template',
-        template=ImageCarouselTemplate(
-        columns=[
-            ImageCarouselColumn(
-                image_url='圖片網址',
-                action=PostbackTemplateAction(
-                    label='postback1',
-                    text='postback text1',
-                    data='action=buy&itemid=1'
-                )
-            ),
-            ImageCarouselColumn(
-                image_url='圖片網址',
-                action=PostbackTemplateAction(
-                    label='postback2',
-                    text='postback text2',
-                    data='action=buy&itemid=2'
-                )
-            )
-        ]
-    )
-    )
-        line_bot_api.reply_message(event.reply_token,Image_Carousel)
-    return 'OK2'
-
+    #message = TextSendMessage(text=event.message.text)
+    #line_bot_api.reply_message(event.reply_token, message)
+    text = event.message.text   
+    #recipeWeb = 'https://icook.tw/recipes/search?q=' + text + '&ingredients='
+    #r = requests.get(recipeWeb)  
+     
+    if text == 'Hi':
+        line_bot_api.reply_message(event.reply_token, 
+        TextSendMessage(text='Hi, mate'))  
+        
+    elif text == 'book':
+        line_bot_api.reply_message(event.reply_token, 
+        TextSendMessage('https://icook.tw/recipes/search?q={}&ingredients='.format(text)))   
+    
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text + '\n學你說話XD'))
+        
+import os
 if __name__ == "__main__":
-    app.run(debug=True,port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
+    
+
+   
+    
+    
+    
+    
