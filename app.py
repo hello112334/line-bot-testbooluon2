0c9202cb7eb3033bb62c19bc7d21ada763d40b87
hello112334 <37241401+hello112334@users.noreply.github.com>
2018-04-06 23:16:23 +0900

    change token

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('b0oOxY7hM4rb8AGClOYmKtPqCIFK004tf55cbZ3QZZNr+/bUZEKve1+flyNG//vRjMHK5xt6XLZcqhrs6B8v5gW6dKC5BIb26wDDfUCekK5AbObpdycypf49s+v3r3IqWVqRWvoQ267sLm2Rk4BO/AdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('PkVllUtV17i8szoq5TE3bG94IPi/wI+z7TcylMT+LPXj+Gy2bwsDrR2R13EeUv5LWSXSAjkZQ8SMmBL1A2hxQH6R+22UozcZgCdTgi9SjFBgqeyjPAjSlqJHqZMWmsQ02q1bOyLPK/tO0GicKD7yrgdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('33741068f54f5270a576abb88087fc3a')
+handler = WebhookHandler('3d5fedfb05e3a26ec8e84b8ca176a11d')
 
 
 # 監聽所有來自 /callback 的 Post Request
